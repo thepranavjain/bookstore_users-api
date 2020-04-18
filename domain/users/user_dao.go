d595ef894b0b6cb87d50483e4988e9c6be4190d4
thepranavjain <pranavjn.kota@gmail.com>
2020-04-18 21:51:45 +0530

    Date utilities

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -3,6 +3,7 @@ package users
 import (
 	"fmt"
 
+	"github.com/thepranavjain/bookstore_users-api/utils/date_utils"
 	"github.com/thepranavjain/bookstore_users-api/utils/errors"
 )
 
@@ -33,6 +34,9 @@ func (user *User) Save() *errors.RestErr {
 		}
 		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
 	}
+
+	user.DateCreated = date_utils.GetNowString()
+
 	usersDB[user.Id] = user
 	return nil
 }
